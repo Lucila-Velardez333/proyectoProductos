2dc77a7a8e8296c3ad5961c3ed49f31e341bacf5
Lucila-Velardez333 <luluvelardez9@gmail.com>
2024-11-11 16:54:32 -0300

    implemente classs de fruta
    implemente class

--- inventarioProductos.py
+++ inventarioProductos.py
@@ -1,16 +1,32 @@
+class Fruta:
+    def __init__(self,nombre,stock):
+        self.nom= nombre
+        self.stock=stock
+
+
 
 print("**************************************")
 print("SELECCIONE UNA OPCION: ")
 print("1- agregar productos al inventario")
 print("2- mostrar los productos registrados")
-print("3- salir")
+print("0- salir")
 print("**************************************")
 
 entrada = int(input("ingrese la opcion: "))
-
+fruta1 = Fruta("naranja",10)
+fruta2 = Fruta("pomelo",3)
+fruta3 = Fruta("sandia",2)
 frutas= [
-    ["naranja",10],["pomelo",3],["sandia",2]
+    fruta1 , fruta2, fruta3
 ]
+k=0
+def Mostrar():
+    if frutas:
+        print("Frutas registradas: ")
+        for fruta in frutas:
+            print(f"{fruta.nom} - Stock: {fruta.stock}")
+    else:
+        print("No hay frutas registradas.")
 
 while entrada!=0 :                                                                                      
 
@@ -18,13 +34,12 @@ while entrada!=0 :
         print("ingrese los productos")  
         nombre =  input("Ingrese nombre de la fruta: ")
         stock =  input("Ingrese el stock de la fruta: ")
-        nueva_fruta = [nombre,stock]
+        nueva_fruta = Fruta(nombre, stock)
         frutas.append(nueva_fruta)
         print("Fruta agregada exitosamente.")
     elif(entrada==2):
         print("Frutas registradas: ")
-        for fruta in frutas:
-            print(f"Fruta:{fruta[0]} - Stock: {fruta[1]}")
+        Mostrar()
 
     elif(entrada== 0):
         print("hasta pronto")
